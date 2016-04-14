e5cbbb2e7af504bd9fe2e144b666e342b4171f0e
utroy <utsavroy@u.boisestate.edu>
2016-04-13 22:06:42 -0600

    Create arthemetic.hs

---
+++ arthemetic.hs
@@ -0,0 +1,13 @@
+-- add 
+addNums :: Int -> Int -> Int ->Int
+addNums x y z = x+y+z
+
+--subtract
+subNums :: Int -> Int -> Int
+subNums x y = x - y
+
+-- multiply 
+multNums = 20 * 30
+
+--Divide 
+divNum = 20/10
